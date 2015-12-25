dce88d3fcd657f6ce882fae2c28f6c7178e7d434
Ragavendra Nagraj <ragavendra.bn@gmail.com>
2015-12-24 21:29:24 -0800

    working copy

--- server.rb
+++ server.rb
@@ -2,57 +2,44 @@ require 'webrick'
 require 'json'
 include WEBrick
 
-DOCKER_PATH = "#{ENV['HOME']}/github/devops/soa/dev"
-API_SPECS_PATH = "#{ENV['HOME']}/github/tests/soa/api"
-UI_SPECS_PATH = "#{ENV['HOME']}/github/tests/soa/se"
-#SPECS_LOGPATH = "#{SPECS_PATH}/logs/#{@repo_name}"
-RAKE_TASK = "cards:dev"
-#; cd $HOME/gitbase/test-auto/SOA/api; bundle exec rake cards:dev | aha --black >$LOGPATH"
+DOCKER_PATH = "#{ENV['HOME']}/gitbase/devops/services/soa/dev"
+#DOCKER_PATH = "#{ENV['HOME']}/gitbase/devops/services/soa/#{@git_branch}"
+API_SPECS_PATH = "#{ENV['HOME']}/gitbase/test-auto/SOA/api"
+UI_SPECS_PATH = "#{ENV['HOME']}/gitbase/test-auto/SOA/se"
 
-=begin
-apiUser.rb
-attr_accessor :autoBankVerificationService
-attr_accessor :responseAssertions
-attr_accessor :memberService
-    
-    def initialize(opts)
-	@opts = opts
-	@memberService = MemberService.new(@opts)
-	@responseAssertions = ResponseAssertions.new(@opts)
+HOST="dockervm"
+#HOST="https://amzn-se001"
+#HOST="https://qa.mogo.ca"
+BROWSER="phantomjs"
+SCREENS=true
 
-specs.rb
-@apiUser = ApiUser.new(@opts)   
-        #puts @opts.inspect             
-		    
-	#signup, activate, login and credit application
-	res = @apiUser.memberService.signup 
-
-=end
 class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 
-	def initialize   
+	def initialize param
 		#def initialize(opts)   
 		#super(opts)    
 		@resourcePath = "/cards"       
 		@repo_name = ''
 		@process_status = ''
+		@result = ''
+		@result_api = ''
+		@result_ui = ''
 		@git_branch = ''
 	end
-#DOCKER_PATH_1_ = '/home/uname/github/devops/soa/dev/'
-#can be called outside class like DockerEndPoint::DOCKER_PATH_1
+
 
 	#curl localhost:8080/docker
 	def do_GET (request, response)
-		
+
 		#status, content_type, body = do_stuff_with request
 		response.status = 200
 		response['Content-Type'] = 'text/plain'
 		response.body = 'Hello, World!'
-		
+
 		puts "this is a get request"
 		#puts "Dir - #{root}"	
 		#puts "Dir - #{File.expand_path '/home/ragavendran@mogo.ca/gitbase/test-auto/SOA/api/log/'}"	
-		
+
 		obj = {
 			"TIME" => Time.now.strftime("%FT%T"),
 			"foo" => "Test",
@@ -72,113 +59,72 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 		response['Content-Type'] = "application/json"
 	end
 
-=begin uncomment if conf token is not in response. This code parses confirmation token from response
+	def do_POST (request, response)
+		puts "this is a post request who received #{request.body}"
+
+		requestHash = JSON.parse(request.body, symbolize_names: true)
+		#repo_name = requestHash[:repository]
+		#repo_name.split('/') repo_name[1]}
+		@repo_name = requestHash[:name]
+		#require 'pry'
+		#binding.pry
+=begin
 		cmd ="docker logs --tail=9 #{@opts[:docker_compose_logger]}"
 		ENV["http_proxy"] = ''
 		Dir.chdir(@opts[:docker_compose_path]){
 			@log_str = %x[#{cmd}]
 		}
-
-		puts "Verify pin pos: " + @log_str.index("verification_pin").to_s unless @log_str.index("verification_pin")
-
-		if @log_str.index("confirmation_token")
-			str_pos =  @log_str.index("confirmation_token")
-			@opts[:user_confirmation_token] = @log_str[(str_pos + 21), 36]
-		end
-		ENV["http_proxy"] = 'http://localhost:8888'
 =end
+
+		#%x["cd #{DOCKER_PATH}"]
 
 =begin
-		{
-			"repository": "mynamespace/repository",
-			"namespace": "mynamespace",
-			"name": "repository",
-			"docker_url": "quay.io/mynamespace/repository",
-			"homepage": "https://quay.io/repository/mynamespace/repository/build?current=some-fake-build",
-			"visibility": "public",
+		Dir.chdir(DOCKER_PATH){
+			cmd = "git pull"
+			@result = %x[#{cmd}]
 
-			"build_id": "build_uuid",
-			"build_name": "some-fake-build",
-			"docker_tags": ["latest", "foo", "bar"],
+			cmd = "sudo docker-compose pull #{@repo_name}"
+			@result = %x[#{cmd}]
 
-			"trigger_kind": "github",                                       #// Optional
-			"trigger_id": "some-id-here",                                   #// Optional
-			"trigger_metadata": {                                           #// Optional
-				"default_branch": "master",
-				"ref": "refs/heads/somebranch",
-				"commit": "42d4a62c53350993ea41069e9f2cfdefb0df097d",
-				"commit_info": {                                              #// Optional
-					"url": "http://path/to/the/commit",
-					"message": "Some commit message",
-					"date": 2395748365,
-					"author": {                                             #    // Optional
-						"username": "fakeauthor",
-						"url": "http://path/to/fake/author/in/scm",               #// Optional
-						"avatar_url": "http://www.gravatar.com/avatar/fakehash"   #// Optional
-					},
-					"committer": {                                           #   // Optional
-						"username": "fakecommitter",
-						"url": "http://path/to/fake/comitter/in/scm",        #     // Optional
-						"avatar_url": "http://www.gravatar.com/avatar/fakehash"   #// Optional
-					}
-				}
-			}
+			cmd = "sudo docker-compose kill" #haproxy may need to be restarted
+			#cmd = "sudo docker-compose kill #{@repo_name}"] #haproxy may need to be restarted
+			@result = %x[#{cmd}]
+
+			#%x[docker-compose rm -y]
+			cmd = "sudo docker-compose up -d"
+			@result = %x[#{cmd}]
+
+			#%x["sudo docker-compose start #{@repo_name}""]
+			cmd = "sudo docker-compose ps"
+			@process_status = %x[#{cmd}]
+			puts "Docker process status - #{@process_status}"
+			#%x[docker-compose ps]
 		}
 =end
-
-	#curl -d "{\"command\":\"ls\", \"par1\":\"/opt\"}" localhost:8080/docker/	
-	def do_POST1 (request, response)
-		puts "this is a post request who received #{request.body}"
-		#repo_name = requestHash[:repository]
-		#repo_name.split('/') repo_name[1]}
- 		@repo_name = requestHash[:name]
-		%x["cd #{DOCKER_PATH}"]
-		%x["git pull"]
-		%x["docker-compose pull"]
-		%x["docker-compose pull #{@repo_name}"]
-		%x["docker-compose kill"]
-		%x["docker-compose kill #{@repo_name}"]
-		#%x[docker-compose rm -y]
-		%x["docker-compose up -d"]
-		%x["docker-compose start #{@repo_name}""]
-		@process_status = %x["docker-compose ps"]
-		puts "Docker process status - #{@process_status}"
-		
-
-		#%x[docker-compose ps]
-		
- 		@git_branch = requestHash[:trigger_metadata][:ref]
+		@git_branch = requestHash[:trigger_metadata][:ref]
 		#git_branch.split('/')"ref": "refs/heads/somebranch"
-		@git_branch = @git_branch[2]
-
+		@git_branch = @git_branch.split('/')
+		@git_branch = @git_branch.last
 		#navigate to specs dir
-		%x["cd #{API_SPECS_PATH}"]
-		%x["bundle exec rake #{@repo_name}:#{@git_branch} | aha --black >logs/#{@git-branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-API-run.htm"]
-		#%x["bundle exec rake cards:dev | aha --black >$LOGPATH/$(date +\%d-\%m-\%Y-\%H-\%s)-run.htm"]
-		
-		#navigate to specs dir
-		%x["cd #{UI_SPECS_PATH}"]
-		%x["bundle exec rake #{@repo_name}:#{@git_branch} | aha --black >logs/#{@git-branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-UI-run.htm"]
-		
-		#requestHash = JSON.parse(request.body, symbolize_names: true)
-		#@opts[:confirmation_token] =  requestHash[:command]
-		#"repository": "mynamespace/repository"
-=begin	
-		##################<SOA - Cards>#################
-		LOGDIR=card-team
+		#%x["cd #{API_SPECS_PATH}"]
 
-		#perform git pull
-		00 17,23,5,11 * * * cd $HOME/gitbase/devops/services/soa/dev; git pull; cd $SPECS_PATH; git pull 2>/tmp/git-pull.log
-		#00 17,23,5,11 * * * cd $DOCKER_PATH; git config --global credential.helper 'cache --timeout 21600'; git pull; cd $SPECS_PATH; git pull 2>/tmp/git-pull.log
+		Dir.chdir(API_SPECS_PATH){
+			cmd = "RUBYOPT='-W0' HOST=#{HOST} RMQ_HOST=#{HOST} RMQ_VHOST=/ bundle exec rake cards:#{@git_branch} | aha --black >logs/#{@git_branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-API-run.htm"
+			@result_api = %x[#{cmd}]
+			#%x["bundle exec rake cards:dev | aha --black >$LOGPATH/$(date +\%d-\%m-\%Y-\%H-\%s)-run.htm"]
+		}
 
-		#run cards
-		00 17 * * 1-5 export LOGPATH=$HOME/gitbase/test-auto/SOA/api/logs/$LOGDIR/$(date +\%d-\%m-\%Y-\%H-\%s)-run.htm; cd $HOME/gitbase/test-auto/SOA/api; bundle exec rake cards:dev | aha --black >$LOGPATH
-		#; bin/slack.sh 2>/tmp/specs-run.log
-		#00 17,21 * * 1-5 cd $HOME/gitbase/test-auto/SOA/api; bin/docker.sh; sleep 15; bundle exec rake cards:all | aha --black >logs/SOACards/$(date +\%d-\%m-\%Y-\%H)-run.htm
-		################</SOA - Cards>################
-=end
+
+		#%x["cd #{UI_SPECS_PATH}"
+		Dir.chdir(UI_SPECS_PATH){
+			#navigate to specs dir
+			cmd = "HOST='https://#{HOST}' BROWSER=phantomjs SCREENS=true bundle exec rake selenium:#{@git_branch} | aha --black >logs/#{@git_branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-UI-run.htm"
+			@result_ui = %x[#{cmd}]
+		}
 		obj = {
-			"Result" => @process_status
+			"Docker logs" => @process_status,
+			"API logs" => @result_api,
+			"SE logs" => @result_ui
 		}
 		response.body = JSON.generate obj
 		response['Content-Type'] = "application/json"
@@ -186,15 +132,15 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 	end	
 
 	#curl -d "{\"command\":\"ls\", \"par1\":\"/opt\"}" localhost:8080/docker/	
-	def do_POST (request, response)
+	def do_POST2 (request, response)
 		puts "this is a post request who received #{request.body}"
-			
+
 		requestHash = JSON.parse(request.body, symbolize_names: true)
 		#@opts[:confirmation_token] =  requestHash[:command]
-		
+
 		#cmd ="#{requestHash[:command]} docker logs --tail=9 #{@opts[:docker_compose_logger]}"
 		cmd ="#{requestHash[:command]} #{requestHash[:par1]}"
-		
+
 		obj = {
 			"Result" => %x[#{cmd}]
 		}
@@ -203,23 +149,17 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 		response['Content-Type'] = "application/json"
 
 	end	
-#parse the response JSON here
-
-		#res = @apiUser.memberService.signup
-		#assert res.code == 200, "Invalid response code #{res}, should be 200-OK"
-#=begin
-		#resHash = JSON.parse(res.body, symbolize_names: true)
-		#@opts[:confirmation_token] =  resHash[:confirmation_token]
-
 end
 
+
+
 #root = File.expand_path '~/gitbase/test-auto/SOA/api/log/'
-root = File.expand_path '/home/ragu/Documents/github/SOA/webrick/www'
+root = File.expand_path "#{API_SPECS_PATH}/logs" 
 
 #server = WEBrick::HTTPServer.new(:Port => 3030)
 server = WEBrick::HTTPServer.new :Port => 8080, :DocumentRoot => root, :DirectoryIndex  => []
 
-server.mount "/docker", DockerEndpoint
+server.mount "/qa/docker/", DockerEndpoint
 
 trap("INT") {
 	server.shutdown
