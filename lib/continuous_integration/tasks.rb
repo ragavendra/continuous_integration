ccc8587aca6fde6b232980fb23ed7d6683703abf
Ragavendra <ragavendra.bn@gmail.com>
2017-11-12 02:06:06 -0800

    adding necessary comments

--- lib/continuous_integration/tasks.rb
+++ lib/continuous_integration/tasks.rb
@@ -3,8 +3,6 @@ require_relative 'constants'
 class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 
 	def initialize param
-		#def initialize(opts)   
-		#super(opts)    
 		@resourcePath = "/cards"       
 		@repo_name = ''
 		@process_status = ''
@@ -17,46 +15,21 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 
 
 	#curl localhost:8080/docker
-	def do_GET (request, response)
-
-		#status, content_type, body = do_stuff_with request
-		response.status = 200
-		response['Content-Type'] = 'text/plain'
-		response.body = 'Hello, World!'
-
-		puts "this is a get request"
-		#puts "Dir - #{root}"	
-		#puts "Dir - #{File.expand_path '/home/raga/gitbase/test-auto/SOA/api/log/'}"	
-
-		obj = {
-			"TIME" => Time.now.strftime("%FT%T"),
-			"foo" => "Test",
-			"bar" => "QA-Automation",
-			'a' => 2,
-			'b' => 3.141,
-			'COUNT' => 1,
-			'c' => 'c',
-			'd' => [ 1, "b", 3.14 ],
-			'e' => { 'foo' => 'bar' },
-			'g' => "Ragu's response",
-			'h' => 1000.0,
-			'i' => 0.001,
-			'j' => "\xf0\xa0\x80\x81",
-		}
-		response.body = JSON.generate obj
-		response['Content-Type'] = "application/json"
-	end
-
+	#POST call made by quay to CI server
 	def do_POST (request, response)
 
+		#parse JSOn to hash key
 		requestHash = JSON.parse(request.body, symbolize_names: true)
+		
+		#fetch the github repo name
 		@repo_name = requestHash[:name]
 
+		#fetch the github branch name
 		@git_branch = requestHash[:trigger_metadata][:ref]
 		@git_branch = @git_branch.split('/')
 		@git_branch = @git_branch.last
 
-		#handle requests one at a time - Needs testing
+		#handle requests one at a time (to avoid concurrency) - Needs testing
 		while @lock 
 			@lock = true
 			docker_update
@@ -70,6 +43,7 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 		end
 	end	
 
+	#update docker images locally
 	def docker_update
 		Dir.chdir(DOCKER_PATH){
 			#perform docker images update
@@ -80,11 +54,13 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 
 	end
 	
+	#Action - run api tests
 	def run_api_tests
 		Dir.chdir(API_SPECS_PATH){
-			cmd = "RUBYOPT='-W0' HOST=#{HOST} RMQ_HOST=#{HOST} RMQ_VHOST=/ bundle exec rake cards:#{@git_branch} | aha --black >logs/#{@git_branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-API-run.htm"
-			@result_api = %x[#{cmd}]
-			#%x["bundle exec rake cards:dev | aha --black >$LOGPATH/$(date +\%d-\%m-\%Y-\%H-\%s)-run.htm"]
+			#shell command to run my tests using rake . I am using `aha` shell tool to grab shell output to html
+			cmd = "RUBYOPT='-W0' HOST=#{HOST} RMQ_HOST=#{HOST} RMQ_VHOST=/ bundle exec rake cars:#{@git_branch} | aha --black >logs/#{@git_branch}/$(date +\%d-\%m-\%Y-\%H-\%s)-API-run.htm"
+			@result_api = %x[#{cmd}] 
+			# or use ` .... ` instead of %x[ .... ] to run commands
 		}
 	end
 	
@@ -97,7 +73,6 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 	end
 
 	def generate_log
-	
 		obj = {
 			"Docker logs" => @process_status,
 			"API logs" => @result_api,
@@ -112,4 +87,44 @@ class DockerEndpoint < WEBrick::HTTPServlet::AbstractServlet
 	end
 end
 
+#Below is the POST from Quay.io, parse and use the necessary data
+=begin
 
+{
+  "repository": "mynamespace/repository",
+  "namespace": "mynamespace",
+  "name": "repository",
+  "docker_url": "quay.io/mynamespace/repository",
+  "homepage": "https://quay.io/repository/mynamespace/repository/build?current=some-fake-build",
+  "visibility": "public",
+
+  "build_id": "build_uuid",
+  "docker_tags": ["latest", "foo", "bar"],
+
+  "trigger_kind": "github",                                       // Optional
+  "trigger_id": "some-id-here",                                   // Optional
+  "trigger_metadata": {                                           // Optional
+    "default_branch": "master",
+    "ref": "refs/heads/somebranch",
+    "commit": "42d4a62c53350993ea41069e9f2cfdefb0df097d",
+    "commit_info": {                                              // Optional
+      "url": "http://path/to/the/commit",
+      "message": "Some commit message",
+      "date": 2395748365,
+      "author": {                                                 // Optional
+        "username": "fakeauthor",
+        "url": "http://path/to/fake/author/in/scm",               // Optional
+        "avatar_url": "http://www.gravatar.com/avatar/fakehash"   // Optional
+      },
+      "committer": {                                              // Optional
+        "username": "fakecommitter",
+        "url": "http://path/to/fake/comitter/in/scm",             // Optional
+        "avatar_url": "http://www.gravatar.com/avatar/fakehash"   // Optional
+      }
+    }
+  }
+}
+
+More info here -> http://docs.quay.io/guides/notifications.html#webhook_build_success
+
+=end
