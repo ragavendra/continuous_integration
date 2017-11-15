e82df3a8e92f719165b219bf5cb6da5df5f0e5c1
ragavendra <ragavendra.bn@gmail.com>
2017-11-15 12:47:49 -0800

    updated to non priv methods

--- test/test_continuous_integration.rb
+++ test/test_continuous_integration.rb
@@ -5,48 +5,31 @@ class ContinuousIntegrationTest < Minitest::Test
 	def setup
 		#@server = ContinuousIntegration.setup_server
 		@root = File.expand_path "#{API_SPECS_PATH}/logs" 
+		@t1 = nil
+		@t2 = nil
+		@server = nil
 	end
 
 	def teardown
-		@server = nil
 	end
 
 	def test_order
 		:alpha
 	end
 	
-	def test_setup_server
-		#@server = ContinuousIntegration.setup_server
-#		assert_kind_of WEBrick::HTTPServer, @server, "Shutdown server did not return object"
-#		@server = ContinuousIntegration.shutdown_server @server
-		#assert_equal "hello world", Hola.hi("english")
-		#@server = ContinuousIntegration.dir_unmount @root
-	end
-	
-	def test_shutdown_server
-		@server = ContinuousIntegration.setup_server
-		t1 = Thread.new { @server = ContinuousIntegration.start_server @server }
+	def test_server_operations
+		until @t1.nil? do
+			@t1.join
+		end
+		@server = ContinuousIntegration.setup_server if (@t1.nil? && @server.nil?) 
 		assert_kind_of WEBrick::HTTPServer, @server, "Shutdown server did not return object"
-		@server = ContinuousIntegration.shutdown_server @server
-		assert_nil @server, "Server class is not nil"
-		assert t1.status, "Thread is still running"
-	end
-=begin	
-	def test_start_server
-		#@server = ContinuousIntegration.start_server @server
-		assert_empty !@server, "Start server did not return object"
-		ContinuousIntegration.shutdown_server @server
-	end
 
-	def test_create_server
-		@server = ContinuousIntegration.create_server @root
-		assert_empty !@server, "Create server did not return object"
+		@t1 = Thread.new { @server = ContinuousIntegration.start_server @server }
+		assert_kind_of WEBrick::HTTPServer, @server, "Shutdown server did not return object #{@server}"
+
+		@t2 = Thread.new { @server = ContinuousIntegration.shutdown_server @server }
+		@t2.join
+		assert_nil @server, "Server class is not nil"
+		assert @t1.status, "Thread is still running #{@t1.status}"
 	end
-	
-	def test_dir_mount
-		@server = ContinuousIntegration.create_server @root
-		@server = ContinuousIntegration.dir_mount @server
-		assert_empty !@server, "Create server did not return object"
-	end	
-=end
 end
