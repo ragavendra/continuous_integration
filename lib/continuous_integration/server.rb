6bf8c70ccdb114f06500084163c6072b98a177c5
ragavendra <ragavendra.bn@gmail.com>
2018-04-02 13:11:49 -0700

    fixing indents

--- lib/continuous_integration/server.rb
+++ lib/continuous_integration/server.rb
@@ -9,63 +9,63 @@ require_relative 'version'
 
 # Module to perform CI operations!
 module ContinuousIntegration
-  # class to perform the server operations
-  class Server
-    # Perform Continuous Integration operations!
-    #
-    # Example:
-    #   >> server = ContinuousIntegration.setup_server
-    #   >> ContinuousIntegration.start_server server
-    #   => INFO  WEBrick x.x.x
-    #
-    # Arguments:
-    #   server: (Object)
+	# class to perform the server operations
+	class Server
+		# Perform Continuous Integration operations!
+		#
+		# Example:
+		#   >> server = ContinuousIntegration.setup_server
+		#   >> ContinuousIntegration.start_server server
+		#   => INFO  WEBrick x.x.x
+		#
+		# Arguments:
+		#   server: (Object)
 
 		attr_accessor	:server, :root
 
 		def self.start_server
 			self.setup_server
 			@server.start
-    end
+		end
 
-    # setup the CI server config
+		# setup the CI server config
 		def self.setup_server
-      # path for the web server to serve the test results
+			# path for the web server to serve the test results
 			@root = File.expand_path "#{API_SPECS_PATH}/logs"
 
-      # create the server
+			# create the server
 			@server = self.create_server
 
-      # mount the dir
+			# mount the dir
 			dir_mount
 
-      # shut server down on any interrupt
-      trap('INT') do
+			# shut server down on any interrupt
+			trap('INT') do
 				shutdown_server
-      end
-    end
+			end
+		end
 
 		def self.shutdown_server
-      dir_unmount
+			dir_unmount
 			@server.shutdown
-    end
+		end
 
-    private_class_method
+		private_class_method
 
 		def self.create_server
-      WEBrick::HTTPServer.new(
-        Port: PORT_NUM,
+			WEBrick::HTTPServer.new(
+				Port: PORT_NUM,
 				DocumentRoot: @root,
-        DirectoryIndex: []
-      )
-    end
+				DirectoryIndex: []
+			)
+		end
 
 		def self.dir_mount
 			@server.mount SUB_URI, DockerEndpoint
-    end
+		end
 
 		def self.dir_unmount
 			@server.unmount SUB_URI
-    end
-  end
+		end
+	end
 end
